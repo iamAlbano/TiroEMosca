2dbc6608300a19ac9d285f67c3fc37b1bfa99916
Guilherme Albano <oguilhermealbano@gmail.com>
2022-09-29 21:44:13 -0300

    update

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = "Filipe Ribeiro, Guilherme Albano, Dani"
 
+import json  
 import tkinter as t
 import socket, sys
-from threading import *
+from threading import Thread
 from random import randint
 
 HOST = '127.0.0.1'  # endereço IP
@@ -13,46 +14,104 @@ BUFFER_SIZE = 1024  # tamanho do buffer para recepção dos dados
 class Game(t.Frame):
 
   def __init__(self, parent):
+      super(Game, self).__init__()
       self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.socket.connect((HOST, PORT))
+      self.tentativa = ''
+      self.turno = None
+      self.minha_sequencia = str(randint(0, 9))+str(randint(0, 9))+str(randint(0, 9))
+      self.pacote = { "tentativa": "", "tiros": 0, "moscas": 0 }
       self.tiros = 0
       self.moscas = 0
-      self.tentativa = ''
-      self.texto_recebido = ''
-      self.numeros = str(randint(0, 9))+str(randint(0, 9))+str(randint(0, 9))
+
       try:
         t.Frame.__init__(self)
 
-        recvThread = Thread(target=self.recebeMsg)
+        # componentes do layout
+        sequencia_label = t.Label(self, text="A sua sequência é: "+self.minha_sequencia)
+        adversario_label = t.Label(self, text="Seu adversário chutou: ")
+        chute_label = t.Label(self, text="Chutar sequência de três números: ")
+        tentativa = t.Entry(self, width=40)
+
+        def enviarTentativa():
+          clientMessage = tentativa.get()
+          self.pacote = { "tentativa": clientMessage }
+          data = json.dumps(self.pacote).encode('utf-8')
+          self.socket.send(data) 
+        
+        def calcularTirosEMoscas(tentativa):
+          numeros = list(tentativa)
+          sequencia = list(self.minha_sequencia)
+          quantidade_tiros = 0
+          quantidade_moscas = 0
+          tiros = []
+          moscas = []
+          for i, numero in enumerate(numeros):
+            for j, valor in enumerate(sequencia):
+              if numero == valor:
+                 if i == j:
+                  quantidade_moscas +=1
+                  moscas.append(numero)
+                  break
+                 else: 
+                  if numero not in moscas and numero not in tiros:
+                    tiros.append(numero)
+                    quantidade_tiros +=1
+
+          enviarFeedback(quantidade_tiros, quantidade_moscas)
+        
+        def enviarFeedback(tiros, moscas):
+          self.pacote = { "tiros":tiros, "moscas": moscas }
+          data = json.dumps(self.pacote).encode('utf-8')
+          self.socket.send(data) 
+
+        btn_enviar = t.Button(self, text="Chutar", width=20, command=enviarTentativa)
+        tiros_label = t.Label(self, text="Tiros: "+str(self.tiros))
+        moscas_label =  t.Label(self, text="Moscas: "+str(self.moscas))
+
+        aguardando = t.Label(self, text="Aguardando adversário...")
+
+        def recebeMsg():
+          while True:
+            data = self.socket.recv(BUFFER_SIZE)
+            pacote_recebido = json.loads(data)
+            if 'turno' in pacote_recebido:
+              self.turno = pacote_recebido['turno']
+            if 'tentativa' in pacote_recebido:
+              adversario_label.configure(text="Seu adversário chutou: "+pacote_recebido['tentativa'])
+              calcularTirosEMoscas(pacote_recebido['tentativa'])
+            if 'tiros' in pacote_recebido:
+              self.tiros = pacote_recebido['tiros']
+              tiros_label.configure(text="Tiros: "+str(self.tiros))
+            if 'moscas' in pacote_recebido:
+              self.moscas = pacote_recebido['moscas']
+              moscas_label.configure(text="Moscas: "+str(self.moscas))
+
+            if 'GAME_OVER' in pacote_recebido:
+                print('vai encerrar o socket cliente!')
+                self.socket.close()
+
+        recvThread = Thread(target=recebeMsg)
         recvThread.daemon = True
         recvThread.start()
+      
+        sequencia_label.grid(column=0, row=0, padx=60, pady=5)
+        adversario_label.grid(column=0, row=1, padx=60, pady=5)
+        chute_label.grid(column=0, row=2, padx=60, pady=5)
         
-        t.Label(self, text="A sua sequência é: "+self.numeros).grid(column=0, row=0, padx=60, pady=5)
-        t.Label(self, text="Seu adversário chutou: "+self.texto_recebido).grid(column=0, row=1, padx=60, pady=5)
-        t.Label(self, text="Chutar número de 0 a 9: ").grid(column=0, row=2, padx=60, pady=5)
-        self.tentativa = t.Entry(self, width=40)
-        self.tentativa.insert(0,"")
-        self.tentativa.grid(column=0, row=3, padx=60, pady=10)
-        btnSendMessage = t.Button(self, text="Chutar", width=20, command=self.enviarMensagem)
-        btnSendMessage.grid(column=0, row=4,padx=10, pady=10)
-        t.Label(self, text="Tiros: "+str(self.tiros)).grid(column=0, row=6, padx=60, pady=5)
-        t.Label(self, text="Moscas: "+str(self.moscas)).grid(column=0, row=7, padx=60, pady=5)
+        tentativa.insert(0,"")
+        tentativa.grid(column=0, row=3, padx=60, pady=10)
+        
+        btn_enviar.grid(column=0, row=4,padx=10, pady=10)
+        
+        tiros_label.grid(column=0, row=6, padx=60, pady=5)
+        moscas_label.grid(column=0, row=7, padx=60, pady=5)
+          
+
       except Exception as error:
         print("Exceção - Programa será encerrado!")
         print(error)
         return
 
-  def enviarMensagem(self):
-      clientMessage = self.tentativa.get()
-      self.socket.send(clientMessage.encode()) #texto.encode - converte a string para bytes
-
-  def recebeMsg(self):
-    while True:
-      data = self.socket.recv(BUFFER_SIZE)
-      self.texto_recebido = repr(data)
-      texto_string = data.decode('utf-8')
-      print('Recebido do servidor', self.texto_recebido)
-      if (texto_string == 'GAME_OVER'):
-          print('vai encerrar o socket cliente!')
-          self.socket.close()
+      
 
--- tcp_server.py
+++ tcp_server.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 __author__ =  "Filipe Ribeiro, Guilherme Albano, Dani"
 
+import json  
 import socket, sys
 from threading import Thread
 
 BUFFER_SIZE = 1024  # tamanho do buffer para recepção dos dados
 
+players = []
+
 def on_new_client(clientsocket,addr):
+    # avisa os jogadores sobre ínicio do jogo e define o primeiro a jogar
+    novo_jogo = json.dumps({ "turno": clientsocket is not players[0]["client"]  if 'adversario' else 'jogador'})
+    clientsocket.send(novo_jogo.encode('utf-8'))
+    print("enviado: ", novo_jogo)
     while True:
         try:
             data = clientsocket.recv(BUFFER_SIZE)
             if not data:
                 break
-            texto_recebido = data.decode('utf-8') # converte os bytes em string
+            texto_recebido = json.loads(data)
             print('recebido do cliente {} na porta {}: {}'.format(addr[0], addr[1],texto_recebido))
-            # envia o mesmo texto ao cliente           
-            clientsocket.send(data)
+            
+            # envia informação aos jogadores          
+            for player in players:
+                if player["client"] is not clientsocket:
+                    player["client"].send(data)
             if (texto_recebido == 'bye'):
                 print('vai encerrar o socket do cliente {} !'.format(addr[0]))
                 clientsocket.close() 
@@ -31,12 +41,16 @@ def main( HOST = '127.0.0.1', PORT = 20000 ):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             server_socket.bind((HOST, PORT))
             server_socket.listen(0)
-            print("Host criado no IP e porta: ", HOST,":",PORT)
             while True:
+                print("Host criado no IP e porta: ", HOST,":",PORT)
                 clientsocket, addr = server_socket.accept()
-                print('Conectado ao cliente no endereço:', addr)
-                t = Thread(target=on_new_client, args=(clientsocket,addr))
-                t.start()   
+                players.append( { "client":clientsocket, "addr":addr} )
+                if len(players) == 2:
+                    for player in players:
+                        print('Conectado ao cliente no endereço:', player["addr"])
+                        t = Thread(target=on_new_client, args=( player["client"], player["addr"]) )
+                        t.start()   
+                    
     except Exception as error:
         print("Erro na execução do servidor")
         print(error)        
